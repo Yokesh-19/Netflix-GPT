4e1d44fbfd23de107a9038ae8011f4e2c51efca6
Yokesh M <727722eucs208@skcet.ac.in>
2026-02-21 23:19:52 +0530

    SignUp/SignIn Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-yellow-100">
-       <h1>Hi Yokesh Here from HashedIn</h1>
-    </div>
+      <Body/>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import Header from './Header'
+
+const Login = () => {
+    const [isSignIn,setISignIn] = useState(true);
+
+  return (
+    <div>
+        <Header/>
+        <div className='absolute'>
+        <img src='https://assets.nflxext.com/ffe/siteui/vlv3/37372b0c-16ef-4614-9c66-f0464ffe4136/web/IN-en-20260216-TRIFECTA-perspective_74aa38a5-f527-417e-a604-a039567a350b_large.jpg' alt='backgroundImg'/>
+        </div>
+        <form className=' w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+            <h1 className='font-bold text-3xl'>{isSignIn ? "Sign In" : "Sign Up"}</h1>
+            <input type='text' placeholder='Email Address' className='p-4 my-4 w-full bg-gray-700'/>
+            <input type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-700'/>
+            <button className='p-4 my-6 bg-red-700 w-full'>Sign In</button>
+            <p className='py-4' onClick={() => setISignIn(false)}>New to Netflix? Sign Up Now</p>
+        </form>
+    </div>
+  )
+}
+
+export default Login
